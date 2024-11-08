586cff1480c55103e3a78699c54322790b728fd4
censuradho <gustavoleite.me@gmail.com>
2024-11-08 13:46:32 -0300

    refactor(CreateReviewDTO): Adjust properties metadata

--- src/domain/dto/Review.dto.ts
+++ src/domain/dto/Review.dto.ts
@@ -1,13 +1,19 @@
-import { IsOptional, IsString } from "class-validator";
+import { IsOptional, IsString, IsUUID, MaxLength } from "class-validator";
 
 export class CreateReviewDTO {
   @IsString()
+  @MaxLength(100)
   jobTitle: string
 
   @IsOptional()
   @IsString()
+  @MaxLength(300)
   description?: string
 
   @IsString()
+  @MaxLength(50)
   category: string
+
+  @IsUUID()
+  company_id: string
 }
