b58426d6de201351f6bafd548c2b8d186545a221
censuradho <gustavoleite.me@gmail.com>
2024-11-11 10:50:40 -0300

    feature(CompanyRepository): Implement findById interface method

--- src/domain/repositories/company/ICompanyRepository.ts
+++ src/domain/repositories/company/ICompanyRepository.ts
@@ -1,5 +1,7 @@
 import { CreateCompanyDTO } from "@/domain/dto/Company.dto";
+import { Company } from "@prisma/client";
 
 export interface ICompanyRepository {
   create (user_id: string, payload: CreateCompanyDTO): Promise<void>
+  findById (id: string): Promise<Company | null>
 }
